f6c31e1daaec25be82e6b9c61bd9317e70a1e14c
phoenix-Force <paradox.joe.007@gmail.com>
2019-09-13 11:52:52 +0530

    second commit

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,7 @@
                     <option value="slide">slide</option>
                   </select>
                   <button class="btn btn-primary" @click = "show=!show">Show</button>
-                <br></br>
+
                 <transition :name= "slct">
                   <div class="alert alert-danger" v-if = "show">Danger!</div>
                 </transition>
@@ -19,6 +19,22 @@
                 <transition name="rte">
                   <div class ="rnd">Rotate</div>
                 </transition>
+                <hr>
+                <button class="btn btn-primary" @click="selectedC=='app-danger'?selectedC='app-success':selectedC='app-danger'">Toggle</button>
+                <hr>
+                <transition name ="fade" mode="out-in">
+                  <component :is="selectedC"></component>
+                </transition>
+                <hr>
+                <input type="text" name="" id="" class="form-control" v-model= "item">
+                <hr>
+                <button class="btn btn-warning" @click= "addItem" >Add</button>
+                <hr>
+                <ol class="list-group">
+                  <transition-group name="slide">
+                     <li class="list-group-item" :key="i" v-for="(i,index) in items" @click="removeItem(index)" >{{i}}</li>
+                  </transition-group>
+                </ol>
 
             </div>
         </div>
@@ -26,16 +42,40 @@
 </template>
 
 <script>
+import danger from "./assets/components/Danger.vue"
+import success from "./assets/components/Success.vue"
     export default {
         data() {
             return {
-              show:false,slct:'slide'
+              item:'',
+              show:false,slct:'slide',
+              selectedC :"app-success",
+              items:['Apple','Banana','Mango','Berry']
             }
+        },
+        components:{
+          appDanger:danger,
+          appSuccess:success
+        },
+        methods:{
+          addItem(){
+            if(this.item ==''){
+              selectedC = 'app-danger'
+            }
+            else{
+              this.items.push(this.item);
+              this.item='';
+            }
+          },
+          removeItem(index){
+            this.items.splice(index,1);
+          }
         }
     }
 </script>
 
 <style scoped>
+
 
   /* v-enter{} */
   .fade-enter{
@@ -44,40 +84,45 @@
   .fade-enter-active{
     transition: opacity 2s;
   }
-  /* .fade-leave{
-    /* opacity:1 */
-
-  .fade-leave-active{
+  .fade-leave{
+     opacity:1;
+     }
+  .fade-leave-acticlickve{
      transition: opacity 2s;
      opacity: 0;
 
   }
+  li{
+    cursor: pointer;
+    color:white;
+    background-color: rgb(62, 9, 112);
+  }
 
   .slide-enter-active{
-    animation: slide-in 1s ease-out forwards;
-    transition: opacity 1s;
+    animation: slide-in .5s ease-out forwards;
+    transition: opacity .5s;
   }
 
   .slide-leave-active{
-    animation: slide-out 1s ease-out forwards;
-    transition: opacity 1s;
+    animation: slide-out .5s ease-out forwards;
+    transition: opacity .5s;
     opacity: 0;
   }
   @keyframes slide-in {
     from{
-      transform: translateY(20px);
+      transform: translateX(100px);
     }
     to{
-       transform: translateY(0);
+       transform: translateX(0);
     }
 
   }
   @keyframes slide-out {
     from{
-      transform: translateY(0);
+      transform: translateX(0);
     }
     to{
-      transform: translateY(20px);
+      transform: translateX(-100px);
     }
   }
 .rnd{
